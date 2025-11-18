ccc05dc7baddff0bc5257c0216e7fefcb9d8385f
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-11-18 22:29:02 +0000

    Refactor cert parse

--- self_signed.go
+++ self_signed.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"log"	
 	"time"
 
@@ -12,10 +13,10 @@ import (
 )
 
 // Create a self-signed certificate, using the given notAfter time
-func createSelfSignedCertificate(notAfter time.Time) (*certificate.Resource, *x509.Certificate, error) {
+func createSelfSignedCertificate(notAfter time.Time) (*certificate.Resource, error) {
 	pub, priv, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	template := x509.Certificate{
@@ -26,23 +27,36 @@ func createSelfSignedCertificate(notAfter time.Time) (*certificate.Resource, *x5
 	}
 	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, pub, priv)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	cert, err := x509.ParseCertificate(derBytes)
-	if err != nil {
-		return nil, nil, err
-	}
-	
 	pkcs8Bytes, err := x509.MarshalPKCS8PrivateKey(priv)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	return &certificate.Resource{
 		Certificate: pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes}),
 		PrivateKey:  pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: pkcs8Bytes}),
-	}, cert, nil
+	}, nil
+}
+
+func createTestCertificate(notAfter time.Time) (*certificate.Resource, *x509.Certificate, error) {
+  certRes, err := createSelfSignedCertificate(notAfter)
+	if err != nil {
+	  return nil, nil, err
+  }
+	
+	cert_pem, _ := pem.Decode(certRes.Certificate)
+	if cert_pem == nil {
+	  return nil, nil, fmt.Errorf("Failed to decode certificate")
+  }
+	
+	parsed_cert, err := x509.ParseCertificate(cert_pem.Bytes)
+	if err != nil {
+	  return nil, nil, err
+	}
+	return certRes, parsed_cert, nil
 }
 
 type SelfSignedClient struct {
@@ -51,7 +65,7 @@ type SelfSignedClient struct {
 func (c *SelfSignedClient) ObtainCertificate(domains []string) (*certificate.Resource, error) {
 	log.Printf("Generating self-signed certificate for %v", domains)
 	notAfter := time.Now().Add(90 * 24 * time.Hour)
-	res, _, err := createSelfSignedCertificate(notAfter)
+	res, err := createSelfSignedCertificate(notAfter)
 	if err != nil {
 		return nil, err
 	}
